0fdd1576bb68d324ddfb091433692761529dfe89
YanisBmdn-EFREI <yanis.boumedine@efrei.net>
2023-10-19 09:50:19 +0200

    Add base app with index and scan listing based on /static/scans directory

--- app/app.py
+++ app/app.py
@@ -1,9 +1,22 @@
+import os
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
+APP_DIRECTORY = "app"
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    base_folder = os.path.join(APP_DIRECTORY, "static", "scans")
+    folders = next(os.walk(base_folder))[1]
+    print(folders)
+    return render_template('index.html', folders=folders)
+
+@app.route('/scans/<string:folder>/')
+def show_images(folder):
+    image_folder = os.path.join(APP_DIRECTORY, "static", "scans", folder)
+    images=os.listdir(image_folder)
+    return render_template('scan.html', folder=folder, images=images)
 
 if __name__ == '__main__':
     app.run(debug=True)
