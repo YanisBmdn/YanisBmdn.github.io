ed6cb293ccb57cf6e79947fae45e5671b238235d
YanisBmdn-EFREI <yanis.boumedine@efrei.net>
2023-10-19 10:33:27 +0200

    Add flake8 and black for linting and formatting

--- app/app.py
+++ app/app.py
@@ -5,17 +5,20 @@ app = Flask(__name__)
 
 APP_DIRECTORY = "app"
 
-@app.route('/')
+
+@app.route("/")
 def index():
     base_folder = os.path.join(APP_DIRECTORY, "static", "scans")
     folders = next(os.walk(base_folder))[1]
-    return render_template('index.html', folders=folders)
+    return render_template("index.html", folders=folders)
 
-@app.route('/scans/<string:folder>/')
+
+@app.route("/scans/<string:folder>/")
 def show_images(folder):
     image_folder = os.path.join(APP_DIRECTORY, "static", "scans", folder)
     images = sorted(os.listdir(image_folder), key=lambda x: int(os.path.splitext(x)[0]))
-    return render_template('scan.html', folder=folder, images=images)
+    return render_template("scan.html", folder=folder, images=images)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
